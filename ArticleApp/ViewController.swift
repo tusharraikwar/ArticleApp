3dc00b4e9bc914bccd740bfe6f87ba5e746c353d
Tushar Raikwar <tushar.raikwar21@gmail.com>
2020-09-30 13:18:40 +0530

    Initial Commit

---
+++ ArticleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArticleApp
+//
+//  Created by Reglaze Technologies on 30/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
